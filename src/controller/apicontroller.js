ddbd00a47d9482c132f2db6b791fd0bed294aaf0
rdavila1604 <raymondcalixto@gmail.com>
2023-08-25 13:43:33 -0500

    prueba

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -25,19 +25,20 @@ const verificar = (req, res) => {
 
 const recibir = (req, res) => {
     try {
-        var entry = (req.body["entry"])[0];
-        var changes = (entry["changes"])[0];
-        var value = changes["value"];
-        var objetoMensaje = value["messages"];
+        // var entry = (req.body["entry"])[0];
+        // var changes = (entry["changes"])[0];
+        // var value = changes["value"];
+        // var objetoMensaje = value["messages"];
 
-        var messages = objetoMensaje[0];
-        var texto = messages["text"]["body"]
+        // var messages = objetoMensaje[0];
+        // var texto = messages["text"]["body"]
 
-        var numero = messages["from"];
+        // var numero = messages["from"];
+        var entry = req.body
 
-        console.log("Enviado desde :" + numero + " El texto es el siguiente : " + texto);
-        enviarmensaje.EnviarMensajeWhastapp(texto,numero);
-
+       // console.log("Enviado desde :" + numero + " El texto es el siguiente : " + texto);
+        //enviarmensaje.EnviarMensajeWhastapp(texto,numero);
+        console.log(entry);
         // console.log(objetoMensaje);
         res.send("EVENT_RECEIVED");
     } catch (e) {
