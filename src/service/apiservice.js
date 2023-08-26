45bd1fd92eff2d06bceef1cbdf6b847f8dd6474d
rdavila1604 <raymondcalixto@gmail.com>
2023-08-25 20:44:17 -0500

    prueba20

--- src/service/apiservice.js
+++ src/service/apiservice.js
@@ -4,7 +4,7 @@ function EnviarMensajeWhastapp(texto,numero){
 
     texto = texto.toLowerCase();
     if (texto.includes("hola")) {
-        var data = JSON.stringify({
+        let data = JSON.stringify({
             "messaging_product": "whatsapp",   
             "recipient_type": "individual",
             "to": numero,
@@ -14,8 +14,20 @@ function EnviarMensajeWhastapp(texto,numero){
                 "body": "🚀 Hola, Como estas, Bienvenido."
             }
         });
+    } else if (texto == 1){
+        let data = JSON.stringify({
+            "messaging_product": "whatsapp",   
+            "recipient_type": "individual",
+            "to": numero,
+            "type": "text",
+            "text": {
+                "preview_url": false,
+                "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu"
+                
+            }
+        });
     } else {
-        var data = JSON.stringify({
+        let data = JSON.stringify({
             "messaging_product": "whatsapp",   
             "recipient_type": "individual",
             "to": numero,
